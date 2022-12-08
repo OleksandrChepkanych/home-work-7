28958d46d2fd0686b84cb855f975c541b6b19f68
OleksandrChepkanych <a.chepkanich@gmail.com>
2022-12-08 14:50:04 +0200

    FIX

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(name='clean-folder',
-      version='0.0.1',
+      version='0.0.3',
       description='Sort fieles in folders',
       author='Olexandr Chepkanych',
       author_email='a.chepkanich@gmail.com',
       license='MIT',
+      packages=find_namespace_packages(),
       entry_points= {'console_scripts': ['clean-folder = clean_folder.clean:normalize']}
       )
